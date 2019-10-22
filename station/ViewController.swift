bd6d40d12459005fea44dd31762631ff392cda4e
RayOtsuka <rayrayo8855@gmail.com>
2019-10-22 18:52:32 +0900

    Initial Commit

---
+++ station/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  station
+//
+//  Created by 大塚嶺 on 2019/10/22.
+//  Copyright © 2019 大塚嶺. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
